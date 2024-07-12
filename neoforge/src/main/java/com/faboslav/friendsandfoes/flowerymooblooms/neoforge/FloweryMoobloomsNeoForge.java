dca7189fc60e0f851f80c15a767c8be413c04573
Aleš Fabiánek <fabianek.ales@gmail.com>
2024-07-12 19:42:32 +0200

    fix

--- neoforge/src/main/java/com/faboslav/friendsandfoes/flowerymooblooms/neoforge/FloweryMoobloomsNeoForge.java
+++ neoforge/src/main/java/com/faboslav/friendsandfoes/flowerymooblooms/neoforge/FloweryMoobloomsNeoForge.java
@@ -2,15 +2,14 @@ package com.faboslav.friendsandfoes.flowerymooblooms.neoforge;
 
 import com.faboslav.friendsandfoes.flowerymooblooms.FloweryMooblooms;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(FloweryMooblooms.MOD_ID)
 public final class FloweryMoobloomsNeoForge
 {
-	public FloweryMoobloomsNeoForge() {
-		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+	public FloweryMoobloomsNeoForge(ModContainer modContainer, IEventBus modEventBus) {
 		modEventBus.addListener(FloweryMoobloomsNeoForge::init);
 	}
 
