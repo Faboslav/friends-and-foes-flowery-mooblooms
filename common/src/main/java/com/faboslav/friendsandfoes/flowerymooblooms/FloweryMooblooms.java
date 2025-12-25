52b4a8501928adbc1406244250b145ac0c99f445
Faboslav <fabianek.ales@gmail.com>
2025-12-25 13:35:22 +0100

    1.21.11

--- common/src/main/java/com/faboslav/friendsandfoes/flowerymooblooms/FloweryMooblooms.java
+++ common/src/main/java/com/faboslav/friendsandfoes/flowerymooblooms/FloweryMooblooms.java
@@ -1,6 +1,6 @@
 package com.faboslav.friendsandfoes.flowerymooblooms;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,8 +9,8 @@ public final class FloweryMooblooms
 	public static final String MOD_ID = "flowerymooblooms";
 	private static final Logger LOGGER = LoggerFactory.getLogger(FloweryMooblooms.MOD_ID);
 
-	public static ResourceLocation makeID(String path) {
-		return ResourceLocation.fromNamespaceAndPath(
+	public static Identifier makeID(String path) {
+		return Identifier.fromNamespaceAndPath(
 			MOD_ID,
 			path
 		);
--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -3,8 +3,19 @@ val IS_CI = System.getenv("CI") == "true"
 plugins {
 	id("dev.kikugie.stonecutter")
 	id("net.neoforged.moddev") version "2.0.115" apply false
-	id("fabric-loom") version "1.11-SNAPSHOT" apply false
+	id("fabric-loom") version "1.14-SNAPSHOT" apply false
+}
+
+
+stonecutter {
+	parameters {
+		filters.exclude("**/*.accesswidener")
+
+		replacements.string(current.parsed >= "1.21.11") {
+			replace("ResourceLocation", "Identifier")
+		}
+	}
 }
 
 if (IS_CI) stonecutter active null
-else stonecutter active "1.21.10" /* [SC] DO NOT EDIT */
+else stonecutter active "1.21.11" /* [SC] DO NOT EDIT */
